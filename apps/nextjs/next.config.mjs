cd5df907ce67fd0088482b448d3d54879525d772
Jackson Kasi <109948871+jacksonkasi1@users.noreply.github.com>
2023-11-01 12:43:58 +0530

    fix

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,17 +1,12 @@
-// @ts-check
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
+/** @type {import('next').NextConfig} */
 
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  transpilePackages: ["@acme/api", "@acme/db"],
+  transpilePackages: ["@repo/server", "@repo/db"],
   // We already do linting on GH actions
   eslint: {
     ignoreDuringBuilds: !!process.env.CI,
@@ -25,4 +20,4 @@ const config = {
   },
 };
 
-export default config;
+module.exports = config;
